135d6f0fb562ba56afaf80723d2cc55fde9ee0f1
Anh-Kiet Ngo <ak@adlightning.com>
2020-02-27 15:58:54 -0800

    Check for originalEval and use that instead if exists

--- json.js
+++ json.js
@@ -243,7 +243,11 @@ exports.parse = function (text, reviver) {
         // in JavaScript: it can begin a block or an object literal. We wrap the text
         // in parens to eliminate the ambiguity.
 
-        j = eval('(' + text + ')');
+        var evalFn = eval;
+        if (window.originalEval) {
+            evalFn = window.originalEval;
+        }
+        j = evalFn('(' + text + ')');
 
         // In the optional fourth stage, we recursively walk the new structure, passing
         // each name/value pair to a reviver function for possible transformation.
